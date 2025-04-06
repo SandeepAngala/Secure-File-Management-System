15a5347fb9f52c3cd570db703818ffabf0055c44
SandeepAngalaSandeepAngala <sandeepbablu265@gmail.com>
2025-04-06 19:34:37 +0530

    some code added

--- gui.py
+++ gui.py
@@ -22,3 +22,20 @@ class App:
         self.root.title("Secure File Management System (MySQL)")
         self.root.geometry("600x500")
         self.show_login_page()
+
+    def show_login_page(self):
+        """Display the login page."""
+        for widget in self.root.winfo_children():
+            widget.destroy()
+        tk.Label(self.root, text="Username").pack(pady=5)
+        self.username_entry = tk.Entry(self.root)
+        self.username_entry.pack(pady=5)
+        tk.Label(self.root, text="Password").pack(pady=5)
+        self.password_entry = tk.Entry(self.root, show="*")
+        self.password_entry.pack(pady=5)
+        tk.Label(self.root, text="TOTP Code").pack(pady=5)
+        self.totp_entry = tk.Entry(self.root)
+        self.totp_entry.pack(pady=5)
+        tk.Button(self.root, text="Login", command=self.login).pack(pady=5)
+        tk.Button(self.root, text="Register", command=self.register).pack(pady=5)
+
